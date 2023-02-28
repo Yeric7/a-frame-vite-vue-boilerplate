f4bf0db8cfc521a338becc83d4b124ff4a466f1b
Yeric7 <y3ric7@gmail.com>
2023-02-28 13:59:03 +0100

    Update grabbable.js

--- src/aframe/grabbable.js
+++ src/aframe/grabbable.js
@@ -1,9 +1,7 @@
 AFRAME.registerComponent('grabbable', {
   schema: {
     target: {type: 'selector', default: '#camera-rig'},
-    offset: {type: 'vec3', default: {x: 0, y: 0.5, z: 0.1}}
   },
-
   init: function () {
     this.initialPos = {...this.el.object3D.position};
     this.targetPos = new THREE.Vector3();
@@ -13,28 +11,19 @@ AFRAME.registerComponent('grabbable', {
     this.target = null;
     this.el.addEventListener('click', evt => this.grab(evt));
   },
-
   grab: function (evt) {
     this.target = this.data.target;
+    this.el.object3D.scale.set(0.5, 0.5, 0.5);
   },
-
   reset: function () {
     this.el.object3D.position.set(this.initialPos.x, this.initialPos.y, this.initialPos.z);
+    this.el.object3D.scale.set(1, 1, 1);
   },
-
   tick: function () {
     if (this.target === null) return;
     // calculate distance to target
     this.data.target.object3D.getWorldPosition(this.targetPos);
-    // offset the grab position based on the asset dimensions
-    const assetBox = new THREE.Box3().setFromObject(this.el.object3D);
-    const assetSize = assetBox.getSize(new THREE.Vector3());
-    this.targetPos.add(new THREE.Vector3(
-      this.data.offset.x * assetSize.x,
-      this.data.offset.y * assetSize.y,
-      this.data.offset.z * assetSize.z
-    ));
-    // and copy rotation
+    // apply the calculated distance to the entity
     this.el.object3D.position.copy(this.targetPos);
     this.el.object3D.rotation.copy(this.data.target.object3D.rotation);
   }
