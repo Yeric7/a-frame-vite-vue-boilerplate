6e3dece3d91c67053219ef6553c965f8b0968c04
Yeric7 <y3ric7@gmail.com>
2023-02-28 15:48:58 +0100

    Test map moove et son

--- src/aframe/grabbable.js
+++ src/aframe/grabbable.js
@@ -1,29 +1,29 @@
 AFRAME.registerComponent('grabbable', {
   schema: {
-    target: { type: 'selector', default: '#camera-rig' },
+    target: {type: 'selector', default: '#camera-rig'},
+    eatSound: {type: 'selector', default: ''},
+    map: {type: 'selector', default: ''},
   },
   init: function () {
-    this.initialPos = { ...this.el.object3D.position };
+    this.initialPos = {...this.el.object3D.position};
     this.targetPos = new THREE.Vector3();
     this.targetRot = new THREE.Quaternion();
     this.pos = new THREE.Vector3();
     this.rot = new THREE.Quaternion();
     this.target = null;
-    this.el.addEventListener('click', (evt) => this.grab(evt));
+    this.eaten = false;
+    this.el.addEventListener('click', evt => this.grab(evt));
   },
   grab: function (evt) {
     this.target = this.data.target;
- 
-    // Scale down the object
-    this.el.object3D.scale.set(0.1, 0.1, 0.1);
-    
+    const boundingBox = new THREE.Box3().setFromObject(this.el.object3D);
+    const center = new THREE.Vector3();
+    boundingBox.getCenter(center);
+    this.el.object3D.position.copy(center);
+    this.el.object3D.scale.set(0.25, 0.25, 0.25);
   },
   reset: function () {
-    this.el.object3D.position.set(
-      this.initialPos.x,
-      this.initialPos.y,
-      this.initialPos.z
-    );
+    this.el.object3D.position.set(this.initialPos.x, this.initialPos.y, this.initialPos.z);
   },
   tick: function () {
     if (this.target === null) return;
@@ -32,5 +32,21 @@ AFRAME.registerComponent('grabbable', {
 
     this.el.object3D.position.copy(this.targetPos);
     this.el.object3D.rotation.copy(this.data.target.object3D.rotation);
-  },
+
+    // Check if the mushroom is colliding with the camera rig
+    const mushroomBB = new THREE.Box3().setFromObject(this.el.object3D);
+    const cameraBB = new THREE.Box3().setFromObject(this.data.target.object3D);
+    if (mushroomBB.intersectsBox(cameraBB)) {
+      if (!this.eaten) {
+        this.eaten = true;
+        if (this.data.eatSound) this.data.eatSound.components.sound.play();
+        if (this.data.map) {
+          setTimeout(() => {
+            const mapPos = this.data.map.object3D.position;
+            this.data.map.setAttribute('animation', `property: position; to: ${mapPos.x} ${mapPos.y} ${mapPos.z + 10}; dur: 5000`);
+          }, 5000);
+        }
+      }
+    }
+  }
 });
--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -74,7 +74,7 @@
       position="-6.633 0.5 -13.754"
       scale="0.5 0.5 0.5"
       animation="property: rotation; to: 0 360 0; loop: true; dur: 3000"
-      grabbable="target:#hand-right"
+      grabbable="target:#hand-right; eatSound: #eat-sound; map: #map1"
       clickable
     ></a-entity>
 <!-- animation="property: rotation; to: 0 360 0; loop: true; dur: 3000"
