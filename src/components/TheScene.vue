878b40281eb2ecf64f7b7fd51ebdfc04fec7693c
Yeric7 <y3ric7@gmail.com>
2023-02-28 11:17:50 +0100

    Update TheScene.vue

--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -33,50 +33,54 @@
 
     <a-assets @loaded="allAssetsLoaded = true">
       <!--
-        Title: VR Gallery
+        Title: Champion Trip
         Model source: https://sketchfab.com/3d-models/vr-gallery-1ac32ed62fdf424498acc146fad31f7e
         Model author: https://sketchfab.com/mvrc.art (Maxim Mavrichev)
         Model license: CC BY 4.0 ( https://creativecommons.org/licenses/by/4.0/ )
       -->
       <a-asset-item  id="room" src="assets/vr_gallery.glb"></a-asset-item>
+
+      <a-asset-item  id="map1" src="assets/MapForestChampi.glb"></a-asset-item>
+      <a-asset-item  id="map2" src="assets/MapAvecAnimaux.glb" ></a-asset-item>
+      <a-asset-item  id="map3" src="assets/ilo.glb" ></a-asset-item>
+      <a-asset-item  id="mushroom" src="assets/glowing_mushroom.glb" ></a-asset-item>
+  
     </a-assets>
 
     <a-entity
       v-if="allAssetsLoaded"
-      gltf-model="#room"
+      gltf-model="#map1"
+      rotation="0 90 0"
+      position="0 -15 -5"
+      scale="1 1.1 1"
+    >    </a-entity>
+
+    <a-entity
+      v-if="allAssetsLoaded"
+      gltf-model="#map2"
+      rotation="0 90 0"
+      position="50 0 -55"
+      scale="1 1.1 1"
+      animation-mixer
+    ></a-entity>
+
+    <a-entity
+      v-if="allAssetsLoaded"
+      gltf-model="#map3"
       rotation="0 90 0"
       position="0 0 -5"
       scale="1 1.1 1"
-    >
-      <a-entity
-        geometry="primitive: plane; height: 2; width: 2;"
-        position="2 2 3.9"
-        rotation="-180 0 0"
-        life-like-automaton="genPerSec: 25;"
-      ></a-entity>
+    >    </a-entity>
 
-      <a-entity
-        geometry="primitive: plane; height: 2; width: 2;"
-        position="2 2 -3.9"
-        life-like-automaton="genPerSec: 25; maxGen: 10; birthRule: 6,7,8; survivalRule: 4,5,6,7,8;"
-      ></a-entity>
-
-      <BoxColorChanging
-        id="box-left"
-        :scale="scale"
-        :color="colorBoxLeft"
-        position="7 0.5 -3"
-        @click="colorBoxRight = randomHsl()"
-      />
-
-      <BoxColorChanging
-        id="box-right"
-        :scale="scale"
-        :color="colorBoxRight"
-        position="7 0.5 3"
-        @click="colorBoxLeft = randomHsl()"
-      />
-    </a-entity>
+    <a-entity
+  v-if="allAssetsLoaded"
+  gltf-model="#mushroom"
+  position="-6.633 0.336 -13.754"
+  scale="0.5 0.5 0.5"
+  animation="property: rotation; to: 0 360 0; loop: true; dur: 3000"
+  grabbable ="target: #hand-right"
+  clickable
+></a-entity>
 
     <TheNavMesh />
 
