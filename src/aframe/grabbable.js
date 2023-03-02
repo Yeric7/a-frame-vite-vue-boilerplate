b650e892f2829ff605ece867cc6ffd2f7132e4bc
Yeric7 <y3ric7@gmail.com>
2023-03-02 17:49:47 +0100

    Update grabbable.js

--- src/aframe/grabbable.js
+++ src/aframe/grabbable.js
@@ -1,7 +1,8 @@
 AFRAME.registerComponent('grabbable', {
   schema: {
     target: { type: 'selector', default: '#camera-rig' },
-    threshold: { type: 'number', default: 2 }
+    threshold: { type: 'number', default: 0.2 },
+    distance: { type: 'number', default: 0.5 },
   },
   init: function () {
     this.initialPos = { ...this.el.object3D.position };
@@ -23,17 +24,25 @@ AFRAME.registerComponent('grabbable', {
   
     // Save a reference to this component for use in the callback
     const self = this;
-  
-    // Check distance to camera rig every 100ms, and emit 'eaten' event if close enough
-    this.interval = setInterval(function() {
-      self.data.target.object3D.getWorldPosition(self.targetPos);
-      self.el.object3D.getWorldPosition(self.pos);
-      const distance = self.pos.distanceTo(self.targetPos);
-      if (distance < 0.1) {
+
+    const head = this.target.getObject3D('head');
+    const hand = this.target.getObject3D('hand-right');
+
+    // Wait for the hand to be close enough to the head to emit the "eaten" event
+    const checkDistance = setInterval(function() {
+      if (hand.position.distanceTo(head.position) < self.data.distance) {
+        clearInterval(checkDistance);
         self.el.emit('eaten');
-        clearInterval(self.interval);
       }
     }, 100);
+
+    // Reset the object's position if it is dropped
+    this.el.sceneEl.addEventListener('grab-end', (evt) => {
+      if (evt.detail.el === this.el) {
+        clearInterval(checkDistance);
+        this.reset();
+      }
+    });
   },
   
   reset: function () {
