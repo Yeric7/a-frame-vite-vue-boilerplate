c71fa2a880c063ea3292328f84353656768f9fe3
Yeric7 <y3ric7@gmail.com>
2023-03-07 16:08:50 +0100

    Sky + animated island + rotate camera

--- src/components/TheCameraRig.vue
+++ src/components/TheCameraRig.vue
@@ -1,16 +1,46 @@
 <script setup>
+  import { ref } from 'vue';
   import '../aframe/disable-in-vr.js';
   import '../aframe/hide-in-vr.js';
   import '../aframe/simple-navmesh-constraint.js';
   import '../aframe/blink-controls.js';
+
+/*   const scale = ref([1, 1, 1]);
+  const rotation = ref([0, 0, 0]);
+  const delay = ref(0);
+
+  setInterval(() => {
+    scale.value = [getRandomFloat(1, 1.5), getRandomFloat(1, 1.5), getRandomFloat(1, 1.5)];
+    rotation.value = [getRandomFloat(0, 0.2), getRandomFloat(0, 0.2), getRandomFloat(0, 0.2)];
+    delay.value = getRandomInt(0, 5000);
+  }, 5000);
+
+  function getRandomFloat(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } */
 </script>
 
 <template>
-  <a-entity
+  <!-- <a-entity
+    id="camera-rig"
+    position="0 0.6 -5"
+    geometry="primitive: box"
+    material="color: blue"
+    :animation__scale="`property: scale; dur: 500; easing: easeInOutSine; dir: alternate; loop: true; to: ${scale.value.join(' ')}; delay: ${delay.value}`"
+    :animation__rotation="`property: rotation; dur: 3000; easing: easeInOutSine; dir: alternate; loop: true; to: ${rotation.value.join(' ')}; delay: ${delay.value + 2000}`"
+    movement-controls="camera: #head; speed: 0.6;"
+  >
+ -->
+ <a-entity
     id="camera-rig"
     movement-controls="camera: #head; speed: 0.6;"
     disable-in-vr="component: movement-controls;"
   >
+        
 
       <a-entity
         id="head"
@@ -48,6 +78,8 @@
         laser-controls="hand: right"
         raycaster="far: 2; objects: [clickable]; showLine: true;"
       ></a-entity>
+
+      
 
   </a-entity>
 </template>
--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, getCurrentInstance  } from "vue";
+import { ref, onMounted, getCurrentInstance } from "vue";
 
 import TheCameraRig from "./TheCameraRig.vue";
 import TheNavMesh from "./TheNavMesh.vue";
@@ -10,7 +10,6 @@ import "../aframe/animation-mixer";
 import "../aframe/life-like-automaton.js";
 import "../aframe/teleport-camera-rig.js";
 import "../aframe/grabbable.js";
-
 
 defineProps({
   scale: Number,
@@ -28,29 +27,75 @@ eatSound.addEventListener("canplaythrough", () => {
 
 const onMushroomGrabbed = () => {
   // Do something when the mushroom is grabbed
-  console.log('Mushroom grabbed!');
-}
+  console.log("Mushroom grabbed!");
+};
+
+const loopScale = (el) => {
+  AFRAME.ANIME({
+    targets: '#camera-rig',
+      // scale: () => `${AFRAME.ANIME.random(0.99, 1)} ${AFRAME.ANIME.random(1,1)} ${AFRAME.ANIME.random(1, 1)}`,
+      // scale: '1.1 1 1',
+      // rotation: '0 1 0',
+      rotation: () => `${AFRAME.ANIME.random(0.99, 1)} ${AFRAME.ANIME.random(0,1)} ${AFRAME.ANIME.random(1, 1)}`,
+      easing: 'easeInOutSine',
+      direction: 'alternate',
+      loop: true,
+    duration: 1000,
+
+  });
+
+  
+};
 
 const onMushroomEaten = () => {
   // Do something when the mushroom is eaten
-  console.log('Mushroom eaten!');
+  console.log("Mushroom eaten!");
 
   if (eatSoundLoaded.value) {
     eatSound.play();
   } else {
-    console.log("Sound not loaded yet. It will be played once it finished loading.");
+    console.log(
+      "Sound not loaded yet. It will be played once it finished loading."
+    );
   }
 
   // Get the map entity and move it
   const mapEntity = instance.refs.mapEntity;
-mapEntity.setAttribute('animation', {
-  property: 'position',
-  to: '0 0 -1155',
-  dur: 10000,
-  easing: 'linear',
-  delay: 2000
+  const mapEntity2 = instance.refs.mapEntity2;
+
+// Move to first position
+mapEntity.setAttribute("animation", {
+  property: "position",
+  to: "0 0 -1155",
+  dur: 2000,
+  easing: "linear",
+  delay: 2000,
 });
-}
+
+// Wait for the animation to complete and then move to second position
+setTimeout(() => {
+  mapEntity.setAttribute("animation", {
+    property: "position",
+    to: "0 -10000 -1155",
+    dur: 100,
+    easing: "linear",
+  }  );
+}, 2500); 
+
+   // Get the 2map entity and move it
+  mapEntity2.setAttribute("animation", {
+    property: "position",
+    to: "0 0 -5",
+    dur: 2500,
+    easing: "linear",
+    delay: 200,
+  });
+
+  // Get the camera rig entity and scale it
+  const cameraRigEntity = document.querySelector("#camera-rig");
+  cameraRigEntity.setAttribute("scale", "1. 1 1");
+  loopScale(cameraRigEntity);
+};
 
 let instance;
 
@@ -58,15 +103,19 @@ onMounted(() => {
   instance = getCurrentInstance();
 });
 
-console.log('animation terminée');
+console.log("animation terminée");
 </script>
 
 
 
+
 <template>
-  <a-scene background="color: black;" renderer="colorManagement: true;" sound>
+  <a-scene  renderer="colorManagement: true;" sound >
+
+    
+    <!-- fog="type: linear; color: #DFE4E6" -->
     <a-assets @loaded="allAssetsLoaded = true">
-     <!--
+      <!--
         Title: Yeric VR
         Model source:
         Map: "The Living Forest" (https://skfb.ly/69WTw) by proxy_doug / (http://creativecommons.org/licenses/by/4.0/).
@@ -76,34 +125,49 @@ console.log('animation terminée');
         "Glowing Mushroom" (https://skfb.ly/otXoo) by rabbiturnal / (http://creativecommons.org/licenses/by/4.0/)
 
       -->
-     
+      <img id="sky" src="assets/sky2.jpg">
       <a-asset-item id="room" src="assets/vr_gallery.glb"></a-asset-item>
 
       <a-asset-item id="map1" src="assets/MapForestChampi.glb"></a-asset-item>
       <a-asset-item id="map2" src="assets/MapAvecAnimaux.glb"></a-asset-item>
       <a-asset-item id="map3" src="assets/ilo.glb"></a-asset-item>
-      <a-asset-item id="mushroom" src="assets/glowing_mushroom.glb" ></a-asset-item>
-     <!--  <audio id="eat-sound" src="assets/mushroomSon.mp3"></audio> -->
-     <a-assets-item v-if="eatSoundLoaded" id="eat-sound" src="#eat-sound"></a-assets-item>
+      <a-asset-item id="cloud1" src="assets/cloud1.glb"></a-asset-item>
+      <a-asset-item id="cloud2" src="assets/cloud2.glb"></a-asset-item>
+      <a-asset-item id="cloud3" src="assets/cloud3.glb"></a-asset-item>
+
+      <a-asset-item
+        id="mushroom"
+        src="assets/glowing_mushroom.glb"
+      ></a-asset-item>
+      <!--  <audio id="eat-sound" src="assets/mushroomSon.mp3"></audio> -->
+      <a-assets-item     v-if="eatSoundLoaded"    id="eat-sound"        src="#eat-sound"      ></a-assets-item>
+
+     <!--  <audio src="https://cdn.aframe.io/basic-guide/audio/backgroundnoise.wav" autoplay
+      preload></audio> -->
+  
     </a-assets>
+   
+    <a-sky src="#sky" v-if="allAssetsLoaded"></a-sky>
+ 
 
     <a-entity
-  v-if="allAssetsLoaded"
-  gltf-model="#map1"
-  rotation="0 90 0"
-  position="0 0 -5"
-  scale="1 1.1 1"
-  ref="mapEntity"
-></a-entity>
-
+      v-if="allAssetsLoaded"
+      gltf-model="#map1"
+      rotation="0 90 0"
+      position="0 0 -5"
+      scale="1 1.1 1"
+      ref="mapEntity"
+      
+    ></a-entity>
 
     <a-entity
       v-if="allAssetsLoaded"
       gltf-model="#map2"
       rotation="0 90 0"
-      position="50 -1 -55"
+      position="0 0 -11155"
       scale="1 1.1 1"
       animation-mixer
+      ref="mapEntity2"
     ></a-entity>
 
     <a-entity
@@ -116,27 +180,40 @@ console.log('animation terminée');
     </a-entity>
 
     <a-entity
-  v-if="allAssetsLoaded"
-  gltf-model="#mushroom"
-  position="-6.633 0.5 -13.754"
-  scale="0.5 0.5 0.5"
-  animation="property: rotation; to: 0 360 0; loop: true; dur: 3000"
-  id="mushroom-entity"
-  grabbable="target: #hand-right;"
-  clickable
-  v-on:grabbed="onMushroomGrabbed"
-  v-on:eaten="onMushroomEaten"
-  sound="src: #eat-sound; on: eaten"
-></a-entity>
+      v-if="allAssetsLoaded"
+      gltf-model="#mushroom"
+      position="-6.633 0.5 -13.754"
+      scale="0.5 0.5 0.5"
+      animation="property: rotation; to: 0 360 0; loop: true; dur: 3000"
+      id="mushroom-entity"
+      grabbable="target: #hand-right;"
+      clickable
+      v-on:grabbed="onMushroomGrabbed"
+      v-on:eaten="onMushroomEaten"
+      sound="src: #eat-sound; on: eaten"
+    ></a-entity>
 
 
 
+<!--     Clouds:
+ --> 
+ <a-entity
+      v-if="allAssetsLoaded"
+      gltf-model="#cloud1"
+      rotation="0 0 0"
+      position="0 0 -35"
+      scale="10 10 10"
+    >
+ </a-entity>
+ 
 
-    <!-- animation="property: rotation; to: 0 360 0; loop: true; dur: 3000"
+ <a-entity geometry="primitive: box" material="envMap: #sky; roughness: 10"></a-entity>  
+
+ <!-- animation="property: rotation; to: 0 360 0; loop: true; dur: 3000"
  -->
     <TheNavMesh />
 
-    <TheCameraRig />
+    <TheCameraRig> </TheCameraRig>
   </a-scene>
 </template>
 
